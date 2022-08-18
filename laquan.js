aa53aeb361269a12694a8e1caffc5c00c488135d
Elvin <Elvin@ElvinGarcia.com>
2022-08-18 22:47:27 +0900

    turn function into IIFE, script propmts the user for input and provides the default value base on whats available, the photosetID is no longer hard coded it is now retrieve from existing photosetId variable in the page

--- laquan.js
+++ laquan.js
@@ -1,21 +1,24 @@
 
 
-
-
-function letItRain(numbOfPic) {
-  let photoDownloadForm = document.getElementById("photoDownloadForm"); //gets the form that gets submit when a request is made
-  let downloadPhotoId = document.getElementById("downloadPhotoId"); // gets the image ID
-  //creates the loop inorder to interrate
+(function () {
+  let photoThumCount = document.querySelector("#photoThumList").childElementCount;
+  // gets the number of images by counting the children inside <ul> tag
+  let numbOfPic = prompt("Please Enter the Amount of Photo to download or press enter for All Photos", photoThumCount);
+  // prompts the user to enter the amount of photos to download
+  let photoDownloadForm = document.getElementById("photoDownloadForm");
+  //gets the form that gets submit when a request is made
+  let downloadPhotoId = document.getElementById("downloadPhotoId");
+  // gets the image ID
+  //creates the loop in order to interrate
   for (let index = 1; index <= numbOfPic; index++) {
-  // a delay per request is required since `setTimeout` is none blocking`
+    // a delay per request is required since `setTimeout` is none blocking`
     setTimeout(() => {
       console.log(index);
       downloadPhotoId.setAttribute(
         "value",
-        `C19-10-1905_${index.toString().padStart(3, 0)}`
+        photosetId.value + `_${index.toString().padStart(3, 0)}`
       );
       photoDownloadForm.submit();
     }, index * 3000);
   }
-}
-
+})();
--- readme.md
+++ readme.md
@@ -1,6 +1,11 @@
 
 ## Downloads All Galary Pics from https://www.laquan.com/.
-* Once signin
-* open developer tool
-* copy and paste the code into the Javascrip console 
-* type `letItRain()` passing the amount of picture you could like to download. eg., `letItRain(112)`
+* Once logged in
+* open your browser's developer console
+  * Firefox `command`+ `option`+ `k` (mac) `Ctrl` + `Shift` + `K` (windows)
+  * Chrome  `option`+ `command`+`j`(mac) `Ctrl`+ `Shift`+ `J` (windows)
+* copy and paste the code into the Javascript console
+* run the script
+
+## Caution
+* Before running the script make sure you have auto save enable or else you will be stuck saving each file
